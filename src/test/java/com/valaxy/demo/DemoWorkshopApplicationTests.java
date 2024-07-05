414e27569c3e2dcd4b90d2117b9c658eedd41ecb
bestdevopsengineer <bestdevopsengineer@gmail.com>
2024-07-05 13:48:38 -0500

    Update DemoWorkshopApplicationTests.java

--- src/test/java/com/valaxy/demo/DemoWorkshopApplicationTests.java
+++ src/test/java/com/valaxy/demo/DemoWorkshopApplicationTests.java
@@ -2,6 +2,8 @@ package com.stalin.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.*;
 
